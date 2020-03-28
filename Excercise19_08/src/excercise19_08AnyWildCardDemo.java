1ad0f2e75db109f6950a049a1b51b838c5d2161c
LcSkelt <59754830+LcSkelt@users.noreply.github.com>
2020-03-28 14:23:07 -0500

    Add files via upload

---
+++ Excercise19_08/src/excercise19_08AnyWildCardDemo.java
@@ -0,0 +1,46 @@
+/*
+Author: Lacee Skelton
+Date: 0320/2020
+Application: Excercise 19-8
+ */
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+public class excercise19_08AnyWildCardDemo extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
