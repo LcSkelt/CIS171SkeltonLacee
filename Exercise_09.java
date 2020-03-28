ab8a0732bb5e9b2243809b1ad414bf43131c750f
LcSkelt <59754830+LcSkelt@users.noreply.github.com>
2020-03-28 17:09:46 -0500

    Add files via upload

---
+++ Exercise_09.java
@@ -0,0 +1,47 @@
+/*
+Author: Lacee Skelton
+Date: 03/25/2020
+Application: Chapter Exercise 19.9
+ */
+
+    import java.util.ArrayList;
+
+
+public class Exercise_09 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        System.out.println(list);
+        System.out.println(list);
+        sort(list);
+        System.out.println(list);
+
+    }
+
+    public static <E extends Comparable<E>> void sort(ArrayList<E> list) {
+
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            E currentMin = list.get(i);
+            int min = i;
+            for (int j = i + 1; j < list.size(); j++) {
+
+                if (list.get(j).compareTo(currentMin) < 0) {
+                    currentMin = list.get(j);
+                    min = j;
+                }
+            }
+
+            if (min != i) {
+                list.set(min, list.get(i));
+                list.set(i, currentMin);
+            }
+        }
+    }
+    
+}
+    
